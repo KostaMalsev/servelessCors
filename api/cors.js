e7afac8d3929e868fbe013bd7e0328a02bff17b1
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-05-08 15:38:15 +0300

    Update cors.js

--- api/cors.js
+++ api/cors.js
@@ -5,12 +5,25 @@ export default async function handler(request, response) {
   
   console.log(request);
   
+  //Get params from the query:
+  var regex = /[?&]([^=#]+)=([^&#]*)/g, params={}, match;
+  
+  while(match = regex.exec(request)) 
+  {
+        params[match[1]] = match[2];
+  }
+  
+  if(params.length == 0) return;
+
   //let url = 'https://www.apple.com/'
-  const {status, data} = await getRequest(decodeURIComponent(request.query.url));
-  //const {status, data} = await getRequest(decodeURIComponent(url));
+  //const {status, data} = await getRequest(decodeURIComponent(request.query.url));
+  const {status, data} = await getRequest(decodeURIComponent(params[0]]));
   
   //let data = request;
   response.status(status).send(data);
+  
+  
+  
   
   
   function getRequest(url) {
