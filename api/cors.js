9a0e3f586fc1ea467f1bd5d6c9f38c13bdf85e92
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-05-09 09:18:10 +0300

    Update cors.js

--- api/cors.js
+++ api/cors.js
@@ -1,44 +1,33 @@
-
 export default async function handler(request, response) {
   
   const https = require('https');
+  let query = Object.entries(request.query);
+  query.shift();
+  let url = request.query.url;
+  query.forEach(entry => {
+    url += '&' + entry[0] + '=' + entry[1];
+  });
   
-  console.log(request);
-    
-  const {status, data} = await getRequest(decodeURIComponent(request.query.url));
+  
+  const {status,data} = await getRequest(url);
   
   response.status(status).send(data);
-  
 
-  
-  
-  
-  
-  
   function getRequest(url) {
-  
-    return new Promise(resolve => {
-      
-      const req = https.get(url, (resp) => {
-        
-        let data = '';
-        
-        resp.on('data', (chunk) => {
-          
-          data += chunk;
-          
-        });
-        
-        resp.on('end', () => {
-          
-          resolve({status: resp.statusCode, data: data});
-          
-        });
-        
-      });
-      
-    });
     
+    return new Promise(resolve => {
+      const req = https.get(url, (resp) => {
+        let data = '';
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+        resp.on('end', () => {
+          resolve({
+            status: resp.statusCode,
+            data: data
+          });
+        });
+      });
+    });
   }
-  
 }
